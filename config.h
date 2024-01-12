0652259b36d3c78e2cc2adfa6921abd84810f026
Dorian <orionthehyena@gmail.com>
2024-01-12 14:33:23 -0700

    updated gaps and browser settings

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 40;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 40;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 40;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 40;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -83,7 +83,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ControlMask,           XK_l,      spawn,          SHCMD("slock") },
-	{ MODKEY,	                XK_b,      spawn,          SHCMD("librewolf") },
+	{ MODKEY,	                XK_b,      spawn,          SHCMD("qutebrowser") },
 	{ MODKEY,                       XK_s,      spawn,          SHCMD("doas brightnessctl s +1%") },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("doas brightnessctl s 1-%") },
 	{ MODKEY|ControlMask,           XK_p,      spawn,          SHCMD("doas poweroff") },
